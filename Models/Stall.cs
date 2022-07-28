82c72cba7b1987998876ab3513d83b80c23733d6
Avinash Singh <avinash_singh2@persistent.com>
2022-07-28 12:24:49 +0530

    Commit made

---
+++ Models/Stall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication15.Models
+{
+    public class Stall
+    {
+        public int costPerDay { get; set; }
+        public int totalNumberDays { get; set; }
+        public string stallType{
+            get; set;
+        }
+
+    }
+}
